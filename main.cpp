20feb15472a292d98b02b9c64e377f4a1d9b0e12
codewithsam110g <68802791+codewithsam110g@users.noreply.github.com>
2022-07-26 12:01:11 +0530

    Part 1 Setup and Window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+#include <glad/glad.h>
+
+#include "window.h"
+#include "Logger.h"
+
+Window* Window::instance;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	Logger::EnableFileOutput("log.txt");
+
+    Window* window = Window::getInstance(800,600, "Phantom Studios");
+
+	gladLoadGL();
+	while (!window->doesWindowClose())
+	{
+		glClearColor(1, 1, 1, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window->getWindowHandle());
+
+	}
+
+	Logger::CloseFileOutput();
+
+	return 0;
 }
--- window.h
+++ window.h
@@ -1,27 +1,110 @@
-#pragma once
-#ifndef CLASS_WINDOW_H
+#define WINDOW_H
+#include "Logger.h"
 #include <GLFW/glfw3.h>
 
-class Window
-{
-public:
-	Window();
-	~Window();
-
+class Window {
 private:
 	GLFWwindow* window;
 	int width, height;
-	const char* tittle;
+	const char* title;
+
+	Window() {
+
+		Logger::Info("Starting to Init Window Object!");
+
+		width = 800;
+		height = 600;
+		title = "Default!";
+
+		if (glfwInit() != GLFW_TRUE) {
+			Logger::Critical("Glfw Cannot Initialize");
+		}
+
+		glfwDefaultWindowHints();
+		window = glfwCreateWindow(width, height, title, NULL, NULL);
+
+		if (window == NULL) {
+			Logger::Error("Could not start window!");
+		}
+
+		glfwMakeContextCurrent(window);
+
+		Logger::Info("Window Object initialized!");
+
+	}
+
+	Window(int new_width, int new_height, const char* new_title) {
+
+		Logger::Info("Starting to Init Window Object!");
+
+		width = new_width;
+		height = new_height;
+		title = new_title;
+
+		if (glfwInit() != GLFW_TRUE) {
+			Logger::Critical("Glfw Cannot Initialize");
+		}
+
+		glfwDefaultWindowHints();
+		window = glfwCreateWindow(width, height, title, NULL, NULL);
+
+		if (window == NULL) {
+			Logger::Error("Could not start window!");
+		}
+
+		glfwMakeContextCurrent(window);
+
+		Logger::Info("Window Object initialized!");
+
+	}
+
+private:
+	static Window* instance;
+
+public:
+	static Window* getInstance() {
+
+		if (!Window::instance) {
+
+			Window::instance = new Window();
+
+		}
+
+		return instance;
+
+
+	}
+
+	static Window* getInstance(int new_width, int new_height, const char* new_title) {
+
+		if (!Window::instance) {
+
+			Window::instance = new Window(new_width, new_height, new_title);
+
+		}
+
+		return instance;
+
+
+	}
+
+	GLFWwindow* getWindowHandle() {
+		return window;
+	}
+
+	bool doesWindowClose() {
+		return glfwWindowShouldClose(window);
+	}
+
+	void setWindowExtents(int new_width, int new_height) {
+		width = new_width;
+		height = new_height;
+		glfwSetWindowSize(window, width, height);
+	}
+
+	void setWindowTitle(const char* new_title) {
+		title = new_title;
+		glfwSetWindowTitle(window, title);
+	}
+
 };
-
-Window::Window(){
-
-	width = 800;
-	height = 600;
-	tittle = "Default!";
-}
-
-Window::~Window(){
-
-}
-#endif // !CLASS_WINDOW_H
